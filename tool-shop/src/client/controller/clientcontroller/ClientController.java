fd0adc1b7311f92f4d8a7e413ae363d0d9cf0217
Patrick Ronn Linang <prlinang@ucalgary.ca>
2020-11-19 18:10:47 -0700

    Created initial IntelliJ project directory and customer management gui

---
+++ tool-shop/src/client/controller/clientcontroller/ClientController.java
@@ -0,0 +1,4 @@
+package client.controller.clientcontroller;
+
+public class ClientController {
+}
---
+++ tool-shop/src/client/view/CustomerManagementGUI.java
@@ -0,0 +1,45 @@
+package client.view;
+
+import javax.swing.*;
+
+public class CustomerManagementGUI {
+
+    private JPanel customerManagementPanel;
+    private JRadioButton customerIDRadioButton;
+    private JRadioButton lastNameRadioButton;
+    private JRadioButton customerTypeRadioButton;
+    private JPanel searchTogglePanel;
+    private JPanel searchCriteriaPanel;
+    private JLabel searchCustomerLabel;
+    private JTextField searachParameterField;
+    private JButton searchButton;
+    private JButton clearSearchButton;
+    private JList searchResultsList;
+    private JScrollPane searchResultsScrollPane;
+    private JPanel searchParameterPanel;
+    private JPanel searchCustomerPanel;
+    private JPanel searchResultsPanel;
+    private JPanel CustomerInfoPanel;
+    private JLabel customerManagementLabel;
+    private JTextField textField2;
+    private JPanel customerAttributesPanel;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+    private JButton saveButton;
+    private JButton deleteButton;
+    private JButton clearButton;
+    private JPanel customerInfoButtons;
+    private JSplitPane customerManagementSplitPanel;
+
+    public CustomerManagementGUI() {
+        JFrame frame = new JFrame("CustomerManagementGUI");
+        frame.setContentPane(this.customerManagementPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        CustomerManagementGUI gui = new CustomerManagementGUI();
+    }
+}
